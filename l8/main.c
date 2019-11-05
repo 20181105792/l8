1c8757105fe5dcc62946a8883866d18efc0a6884
L20181105792 <54578181+L20181105792@users.noreply.github.com>
2019-11-05 15:51:16 +0800

    Initial Commit

---
+++ l8/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  l8
+//
+//  Created by L20181105792 on 2019/11/5.
+//  Copyright © 2019 L20181105792. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
